21ab61ff66aaec9a94ac702f23a7cc9d69f49e0c
Yamini <168359174+yaminipenugonda@users.noreply.github.com>
2024-11-27 11:28:22 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionResultInMVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job {  get; set; }
+        public double Salary {  get; set; }
+        public bool Status {  get; set; }
+    }
+}
---
+++ ResultsController.cs
@@ -0,0 +1,26 @@
+﻿using ActionResultInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultInMVC.Controllers
+{
+    public class ResultsController : Controller
+    {
+       public JsonResult GetEmployees()
+        {
+            Employee e1 = new Employee { Id = 101, Name = "Yamini", Job = "Manager", Salary = 50000, Status = true };
+            Employee e2 = new Employee { Id = 102, Name = "Chinni", Job = "Manager", Salary = 50000, Status = true };
+            Employee e3 = new Employee { Id = 103, Name = "Varun", Job = "Manager", Salary = 50000, Status = true };
+            Employee e4 = new Employee { Id = 104, Name = "Tishika", Job = "Manager", Salary = 50000, Status = true };
+            List<Employee> Emps = new List<Employee> { e1,e2,e3,e4};
+            return Json(Emps, JsonRequestBehavior.AllowGet);
+        }
+        public FileResult DownloadWord()
+        {
+            return File("~/Downloads/Yamini Resume.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+        }
+    }
+}
